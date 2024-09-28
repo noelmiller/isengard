88fe5297998892c29236527c60e10d401661580d
Noel Miller <noelmiller@protonmail.com>
2024-09-27 23:04:29 -0500

    fix: add -i

--- scripts/configure_gnome.sh
+++ scripts/configure_gnome.sh
@@ -2,7 +2,7 @@
 
 set -ouex pipefail
 
-sed "/^favorite-apps=/s/\[.*\]/['org.gnome.Ptyxis.desktop', 'io.github.zen_browser.zen.desktop', 'org.gnome.Nautilus.desktop', 'dev.zed.Zed.desktop', 'steam.desktop', 'org.gnome.Software.desktop']/" /etc/dconf/db/local.d/03-bazzite-dash
+sed -i "/^favorite-apps=/s/\[.*\]/['org.gnome.Ptyxis.desktop', 'io.github.zen_browser.zen.desktop', 'org.gnome.Nautilus.desktop', 'dev.zed.Zed.desktop', 'steam.desktop', 'org.gnome.Software.desktop']/" /etc/dconf/db/local.d/03-bazzite-dash
 
 sed -i 's/tap-to-click=true/tap-to-click=false/' /etc/dconf/db/gdm.d/02-bazzite-global
 
