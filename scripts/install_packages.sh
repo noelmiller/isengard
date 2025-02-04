cd41a7ede0133fb1a30a527b87ad0b0db8877fa2
Noel Miller <4983138+noelmiller@users.noreply.github.com>
2025-02-04 11:58:00 -0600

    Revert "testing dnf5 (#58)" (#59)
    This reverts commit f387337ffd116e63513902817d7ed15fae913eaa.

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -23,15 +23,14 @@ sysadmin_packages=(
 programming_packages=(
   "code"
   "gh"
+  "ghostty"
   "insync"
   "nodejs"
   "zed"
 )
 
-# including firefox because vscode needs it
 utility_packages=(
   "cosmic-store"
-  "firefox"
   "syncthing"
   "stow"
   "scrcpy"
@@ -54,7 +53,7 @@ packages=(
 )
 
 # install rpms
-dnf5 install -y ${packages[@]}
+rpm-ostree install ${packages[@]}
 
 # install fzf-tab-completion
 git clone https://github.com/lincheney/fzf-tab-completion.git /usr/share/ublue-os/fzf-tab-completion
