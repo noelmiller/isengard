14460b9f6d1165e903cd5cb76d347fdb38ea040c
Noel Miller <4983138+noelmiller@users.noreply.github.com>
2025-02-06 13:01:54 -0600

    DNF5 (#60)
    * change stable to testing
    
    * use dnf5 install for 1password
    
    * use dnf5 install to install packages
    
    * adding firefox because vscode is stupid
    
    * update to latest build-container-installer
    
    * add logic for testing
    
    * set ARG_IMAGE_TAG
    
    * fix zen browser ref
    
    * add concurrency and add testing
    
    * remove if pull_request
    
    * fix enrollment password
    
    * use variable expansion

--- scripts/install_1password.sh
+++ scripts/install_1password.sh
@@ -42,7 +42,7 @@ EOF
 rpm --import https://downloads.1password.com/linux/keys/1password.asc
 
 # Now let's install the packages.
-rpm-ostree install 1password 1password-cli
+dnf5 -y install 1password 1password-cli
 
 # Clean up the yum repo (updates are baked into new images)
 rm /etc/yum.repos.d/1password.repo -f
