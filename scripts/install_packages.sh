93420b07cee2549a6eee88fadd21322d17063e76
Noel Miller <noel@noelmiller.dev>
2025-04-17 10:56:37 -0500

    install obs rpm

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -39,6 +39,13 @@ utility_packages=(
   "scrcpy"
 )
 
+obs_packages=(
+  "obs-studio"
+  "obs-studio-plugin-vaapi"
+  "obs-studio-plugin-webkitgtk"
+  "obs-studio-plugin-vkcapture"
+)
+
 docker_packages=(
   "docker-ce"
   "docker-ce-cli"
@@ -52,35 +59,8 @@ packages=(
   ${programming_packages[@]}
   ${utility_packages[@]}
   ${docker_packages[@]}
+  ${obs_packages[@]}
 )
-
-# v4l2loopback
-dnf5 remove v4l2loopback -y
-
-KERNEL_SUFFIX=""
-QUALIFIED_KERNEL="$(rpm -qa | grep -P 'kernel-(|'"$KERNEL_SUFFIX"'-)(\d+.\d+.\d+)' | sed -E 's/kernel-(|'"$KERNEL_SUFFIX"'-)//' | tail -n 1)"
-
-dnf -y install dkms gcc-c++
-
-# The nvidia-open driver tries to use the kernel from the host. (uname -r), just override it and let it do whatever otherwise
-# FIXME: remove this workaround please at some point
-cat >/tmp/fake-uname <<EOF
-#!/usr/bin/env bash
-
-if [ "\$1" == "-r" ] ; then
-  echo ${QUALIFIED_KERNEL}
-  exit 0
-fi
-
-exec /usr/bin/uname \$@
-EOF
-install -Dm0755 /tmp/fake-uname /tmp/bin/uname
-
-curl -fSsL https://github.com/v4l2loopback/v4l2loopback/archive/refs/tags/v0.13.2.tar.gz | tar xfz - 
-cd v4l2loopback-0.13.2
-PATH=/tmp/bin:$PATH make
-PATH=/tmp/bin:$PATH make install
-PATH=/tmp/bin:$PATH depmod -a
 
 # install rpms
 dnf5 install -y ${packages[@]}
